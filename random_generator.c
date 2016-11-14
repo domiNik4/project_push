01d33b957ade88d412f4dc70b4ef226d8bad11c4
domiNik4 <dominika.fric@akademiasovy.sk>
2016-11-14 10:22:59 +0100

    fixed name of the function, changed output parameters

--- random_generator.c
+++ random_generator.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-int* generator(int);
+int* Generator(int);
 
 main()
 {
 	int n;
 	printf("Enter length of array:\n");
 	scanf("%d",&n);
-	int* result=generator(n);
+	int* result=Generator(n);
 	int i;
 	
 	for(i=0;i<n;i++){ //printing the array
@@ -22,17 +22,17 @@ main()
 	
 }
 
-int* generator(int n)
+int* Generator(int n)
 {
 	srand(time(NULL));
 	int i,j;
 	int *p_arr;
 	p_arr=(int*)malloc(sizeof(int)*n); //generating the array of n ints
 	for (i=0;i<n;i++){
-		*(p_arr+i)=rand()%100;
+		*(p_arr+i)=rand()%99+1;
 		for(j=0;j<n;j++){
 			if(*(p_arr+i)==*(p_arr+j)){
-				*(p_arr+j)=rand()%100;
+				*(p_arr+j)=rand()%99+1;
 			}
 		}
 	}
